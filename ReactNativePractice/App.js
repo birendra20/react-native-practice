13e668ef401283251ee00ab4e34ac22c76077c9b
birendra20 <birendragupta20@gmail.com>
2022-09-25 09:21:56 +0530

    first commit

---
+++ ReactNativePractice/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import PizzaTranslator from './component/HandlingTextInput';
+
+const App = () => {
+  return (
+    <View>
+      <PizzaTranslator />
+    </View>
+  );
+};
+
+export default App;
